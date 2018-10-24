7b2f1cf041d6748b55e8fc676be929cfcfa57014
Sébastien Lerique <sl@mehho.net>
2018-10-24 10:43:50 +0200

    Make directory-agnostic so we can call from a symlink

--- lipstat.sh
+++ lipstat.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+BASEDIR=$(dirname $(readlink -f $0))
+NODES=$BASEDIR/nodes.sh
+NODESTAT=$BASEDIR/nodestat.sh
+
 print_nodes_stat () {
-  header="%User,%Nice,Mem (Free),Mem (Cache),Local Disk (Free),$(./nodes.sh --header $1 | sed 's/|/,/g')"
-  ./nodes.sh $1 \
-    | parallel -j0 "echo \$(./nodestat.sh {= s/\|.*//g =} $2)\"|\"{}" \
+  header="%User,%Nice,Mem (Free),Mem (Cache),Local Disk (Free),$($NODES --header $1 | sed 's/|/,/g')"
+  $NODES $1 \
+    | parallel -j0 "echo \$($NODESTAT {= s/\|.*//g =} $2)\"|\"{}" \
     | sed 's/^|/-|-|-|-|-|/g' \
     | sort -t '|' -k 1n,2n \
     | column -t -s '|' -N "$header" -R "%User,%Nice,Mem (Free),Mem (Cache),Local Disk (Free)"
